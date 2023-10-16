f9187df6733f19586c346dc3d0ae0954acc6ef33
L344S <7474@holbertonstudents.com>
2023-10-16 10:25:53 -0700

    test

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+* main - entry point
+* Description: 'main function'
+* @parameter: describe the parameter(none)
+* Return: 0 for success
+*/
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
