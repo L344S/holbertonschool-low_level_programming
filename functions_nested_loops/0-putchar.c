ecb4ec46ca29cc7ab047c23d0b9b324581e1a2f5
L344S <7474@holbertonstudents.com>
2023-10-17 06:19:49 -0700

    correction

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,34 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - entry point
- * Description: This function prints the string "_putchar"
- * Return: Always returns 0
-*/
+ *
+ * Return: 0
+ */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-return (0);
-}
-
-/**
-* _putchar - Writes a character to the standard output.
-* @l: The character to be written.
-* Description: This function writes the character @l to the standard output.
-* Return: Upon success, it returns the number of characters written (1).
-* On error, it returns -1 and sets the errno to indicate the error.
-*/
-int _putchar(char l)
-{
-return (write(1, &l, 1));
+	return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,13 +2,7 @@
 #define MAIN_H
 #include <unistd.h>
 
-/**
-* _putchar - writes the character c to stdout
-* @l: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char l);
+/*0-putchar.c*/
+int _putchar(char c);
 
 #endif
