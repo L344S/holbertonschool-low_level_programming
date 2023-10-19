eff5dc250b13b6244527d8e331d5ff1b6fa8d573
L344S <7474@holbertonstudents.com>
2023-10-19 11:49:40 -0700

    Correct prints the numbers, from 0 to 9, followed by a new line.

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,6 +2,9 @@
 #define MAIN_H
 #include <unistd.h>
 
+/*_putchar.c*/
+int _putchar(char c);
+
 /*0-isupper.c*/
 int _isupper(int c);
 
@@ -10,5 +13,8 @@ int _isdigit(int c);
 
 /*2-mul.c*/
 int mul(int a, int b);
+
+/*3-print_numbers.c*/
+void print_numbers(void);
 
 #endif
