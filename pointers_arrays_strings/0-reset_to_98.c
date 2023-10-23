5b059a3a4b49e03803b65140ca3fef911955085d
L344S <7474@holbertonstudents.com>
2023-10-23 00:45:04 -0700

    work in local : Updates the value it points to to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check if c is digit
+ *
+ * @*n: pointer
+ *
+ * Description: updates the value it points to to 98.
+ * Return: 0 if success
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+
+/*_putchar.c*/
+int _putchar(char c);
+
+/*0-reset_to_98.c*/
+void reset_to_98(int *n);
+
+#endif
