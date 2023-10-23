aa0f23ac84a5756a42ce0c2ecb1926be5491874d
L344S <7474@holbertonstudents.com>
2023-10-23 00:57:30 -0700

    work in local : function that swaps the values of two integers.

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - entry point
+ *
+ * @a: int variable 
+ * @b: int variable 
+ * Description: swaps the values of two integers.
+ * Return: 0 if success
+*/
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
