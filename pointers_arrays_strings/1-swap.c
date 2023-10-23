6af6af396b1fbdba07c3b33d81b941a46dd18f72
L344S <7474@holbertonstudents.com>
2023-10-23 01:00:29 -0700

    work in local : correction betty style + wrong output

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,14 +4,14 @@
 /**
  * swap_int - entry point
  *
- * @a: int variable 
- * @b: int variable 
+ * @a: int variable
+ * @b: int variable
  * Description: swaps the values of two integers.
  * Return: 0 if success
 */
 
 void swap_int(int *a, int *b)
 {
-*a = 42;
-*b = 98;
+*a = *b;
+*b = *a;
 }
