67417748d4fc83843990556d392a6157b296d02b
L344S <7474@holbertonstudents.com>
2023-10-27 08:11:37 -0700

    yeniden yapılmalı

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - entry point
+ * @string: pointer
+ * Description: function that changes
+ * all lowercase letters of a string to uppercase.
+ * Return: convert string if success
+ */
+char *leet(char *str){
+
+char*ptr=str;
+
+while(*str){
+
+char c=*str;
+
+if((c=='a'||c=='A')||(c=='e'||c=='E')||(c=='o'||c=='O')||(c=='t'||c=='T')||(c=='l'||c=='L'))
+{
+if(c=='a'||c=='A')*str='4';
+else if(c=='e'||c=='E')*str='3';
+else if(c=='o'||c=='O')*str='0';
+else if(c=='t'||c=='T')*str='7';
+else if(c=='l'||c=='L')*str='1';
+}
+str++;
+}
+return ptr;
+}
+
