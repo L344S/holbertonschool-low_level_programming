78ef0be598da39835c498cc2bd2746685061b4a4
L344S <7474@holbertonstudents.com>
2023-10-27 08:15:33 -0700

    yeniden yapılmalı

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,24 +7,23 @@
  * all lowercase letters of a string to uppercase.
  * Return: convert string if success
  */
-char *leet(char *str){
+char *leet(char *str) {
+    char *ptr = str;
 
-char*ptr=str;
+    while (*str) {
+        if (*str == 'a' || *str == 'A')
+            *str = '4';
+        else if (*str == 'e' || *str == 'E')
+            *str = '3';
+        else if (*str == 'o' || *str == 'O')
+            *str = '0';
+        else if (*str == 't' || *str == 'T')
+            *str = '7';
+        else if (*str == 'l' || *str == 'L')
+            *str = '1';
+        str++;
+    }
 
-while(*str){
-
-char c=*str;
-
-if((c=='a'||c=='A')||(c=='e'||c=='E')||(c=='o'||c=='O')||(c=='t'||c=='T')||(c=='l'||c=='L'))
-{
-if(c=='a'||c=='A')*str='4';
-else if(c=='e'||c=='E')*str='3';
-else if(c=='o'||c=='O')*str='0';
-else if(c=='t'||c=='T')*str='7';
-else if(c=='l'||c=='L')*str='1';
-}
-str++;
-}
-return ptr;
+    return ptr;
 }
 
