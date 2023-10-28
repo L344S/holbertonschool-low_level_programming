b0bdd673664773f4293278283bdc4714691125fe
L344S <7474@holbertonstudents.com>
2023-10-28 00:01:25 -0700

    yeniden yapılmalı

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -2,28 +2,23 @@
 #include <stdio.h>
 /**
  * leet - entry point
- * @string: pointer
- * Description: function that changes
- * all lowercase letters of a string to uppercase.
+ * @s: pointer
+ * Description: function that encodes a string into 1337.
  * Return: convert string if success
  */
-char *leet(char *str) {
-    char *ptr = str;
+char *leet(char *s)
+{
+int i, j;
+char a[] = "aAeEoOtTlL";
+char b[] = "4433007711";
 
-    while (*str) {
-        if (*str == 'a' || *str == 'A')
-            *str = '4';
-        else if (*str == 'e' || *str == 'E')
-            *str = '3';
-        else if (*str == 'o' || *str == 'O')
-            *str = '0';
-        else if (*str == 't' || *str == 'T')
-            *str = '7';
-        else if (*str == 'l' || *str == 'L')
-            *str = '1';
-        str++;
-    }
-
-    return ptr;
+for (i = 0; *(s + i); i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (a[j] == s[i])
+s[i] = b[j];
 }
-
+}
+return (s);
+}
