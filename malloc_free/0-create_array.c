f3afff2e7d4e58c71690be55c159929d86cd85f7
L344S <7474@holbertonstudents.com>
2023-11-06 08:13:04 -0800

    Work in local : add comments, README and Header files

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,26 +3,34 @@
 #include <stdlib.h>
 /**
  * create_array - entry point
- * @size: size of the array
- * @c: string var 
+ * @size: is the the number of character @c in our array
+ * @c: character that will fill our array
  * Description: function that creates an array of chars
- * Return: Null if size is 0
+ * Return: Null if size is 0, return pointer if success
  */
 char *create_array(unsigned int size, char c)
 {
 
-int i;
-char *test; 
+	int i;
+	char *tableau;
 
-if (size == 0)
-return (NULL);	
+	if (size == 0)
+		return (NULL);
 
-test = malloc(size * sizeof(char));
-if (test == 0)
-return (NULL);
+	tableau = malloc(size * sizeof(char));
+	if (tableau == 0)
+		return (NULL);
 
-for (i = 0; i != '\0'; i++)
-test[i] = c;
+	for (i = 0; i != '\0'; i++)
+		tableau[i] = c;
 
-return (test);
+	return (tableau);
 }
+/**
+ * create_array - betty style architecture
+ * i will help us count and tableau will contain our c chars
+ * before we start, we check if array's size is zero if so no need to do it
+ * with malloc we make some space for our array, return null if no space
+ * we finally start filling our array with c until we've filled everyting
+ * when we're done we return our new array with all he contains
+ */
