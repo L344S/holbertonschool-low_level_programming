031738e14ff6fc2c840d39d5b8be8babc211a784
L344S <7474@holbertonstudents.com>
2023-11-07 22:41:10 -0800

    test

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,10 +21,10 @@ int **alloc_grid(int width, int height)
 	tableau = malloc(height * sizeof(int *));
 	if (tableau == NULL)
 		return (NULL);
-
 	for (i = 0; i < height; i++)
 	{
-		tableau[i] = malloc(width * sizeof(int));
+		tableau[i] = (int *)malloc(width * sizeof(int));
+
 		if (tableau[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
@@ -36,17 +36,3 @@ int **alloc_grid(int width, int height)
 
 	return (tableau);
 }
-/**
- * alloc_grid - code explaination comments
- * i and j variables helps us count in our loop
- * tableau is a container that will hold our multi dimensional array
- * we check if width and height not inferior, if so we return NULL
- * we make some space for enough row that height represent
- * if the computer fails to make space then return NULL
- * first loop to go throught height
- * second loop to go throught width
- * we're making space for our current row in the loop -> tableau[i]
- * giving him enough space for width number
- * if we fail making space then return NULL means we can't make the grid
- * we return our complete grid store in tableau
- */
