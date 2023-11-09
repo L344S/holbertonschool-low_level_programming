a572e2594f5cd98f5ce1ba8d489863d5746f7ebc
L344S <7474@holbertonstudents.com>
2023-11-09 02:25:31 -0800

    Work in local : betty correct

---
+++ preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description:  function that print the filename
+ * return: 0 if success
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
