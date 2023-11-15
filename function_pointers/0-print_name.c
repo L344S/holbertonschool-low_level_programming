693bedf1b1bdf8fc606d601cb94926671f1083b3
L344S <7474@holbertonstudents.com>
2023-11-15 02:12:32 -0800

    attempt to fix checker issues

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,15 +1,13 @@
-#include "function_pointers.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include "function_pointers.h"
 /**
- * print_name - entry point
- * @name: name of the person -> string
- * @f: pointer to an function here its print_name_as_is & print_name_uppercase
- * description: function that prints a name using function in the file 0-main.c
- * return: nothing (void function)
+ * print_name - print the name
+ * @name: name to print
+ * @f: function to use
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL && f == NULL)
-		return;
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
