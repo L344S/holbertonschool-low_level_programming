3c7a10778ee8bee3342e45e1c94223dec186773d
L344S <7474@holbertonstudents.com>
2023-11-15 06:16:27 -0800

    Attempt to fix error

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,11 +12,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size != 0 || array != NULL || cmp != NULL)
+	if (array != NULL || cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 
-			if (cmp(array[i]))
+			if (cmp(array[i]) != 0)
 			{
 				return (i);
 			}
