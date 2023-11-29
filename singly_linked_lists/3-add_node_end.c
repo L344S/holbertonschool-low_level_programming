13a56aa51dbf891aa0277f0b652777f5e593b2e9
L344S <7474@holbertonstudents.com>
2023-11-29 14:25:36 +0100

    Implemented add_node_end function in 3-add_node_end.c
    Function that adds a new node at the end of a list_t list (the function has been tested locally and adheres to the Betty coding style)

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,51 +1,53 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
 /**
-  * add_node_end - adds a new node at the end of a list_t list
-  * @head: pointer to pointer to head of list
-  * @str: string to duplicate to new_node
-  * Return: address of new element or NULL if failed
-  */
+* add_node_end - ajoute un nouveau nœud à la fin d'une liste list_t
+* @head: pointeur vers un pointeur de la tête de la liste
+* @str: chaîne à dupliquer dans new_node
+* Return: adresse du nouvel élément ou NULL en cas d'échec
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-    /* variable qui pointe vers le nouveau node */
+	/* variable qui pointe vers le nouveau nœud */
 	list_t *new_node;
-    /* variable qui sert a parcourir la liste */
+	/* variable qui sert à parcourir la liste */
 	list_t *duplicate;
-    /* longueur de la chaine de caractere */
-	int lenght;
-    /* check si head est NULL */
+	/* longueur de la chaîne de caractères */
+	int length;
+	/* vérifie si head est NULL */
 	if (head == NULL)
 		return (NULL);
-    /* allocation de memoire pour le nouveau node */
+	/* allocation de mémoire pour le nouveau nœud */
 	new_node = malloc(sizeof(list_t));
-    /* check si l'allocation a échoué */
+	/* vérifie si l'allocation a échoué */
 	if (new_node == NULL)
 		return (NULL);
-		if (str) /* check si str est pas NULL */
-		new_node->str = strdup(str); /* si str est NULL, new_node->str est NULL */
-		else
-		new_node->str = NULL; /* sinon, on duplique str dans new_node->str */
-    /* on calcule la longueur de la chaine de caractere */
-	for (lenght = 0; *(str + lenght);)
-		lenght++;
-    /* on assigne la longueur de la chaine de caractere a new_node->len */
-	new_node->len = lenght;
-    /* si head est NULL, on assigne new_node a head */
+	new_node->str = strdup(str);
+	/* vérifie si la duplication de la chaîne a réussi */
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	/* calcule la longueur de la chaîne de caractères */
+	for (length = 0; str[length]; length++)
+	;
+	/* assigne la longueur de la chaîne de caractères à new_node->len */
+	new_node->len = length;
+	new_node->next = NULL;
+	/* si head est NULL, assigne new_node à head */
 	if (*head == NULL)
 	{
 		*head = new_node;
 		return (new_node);
 	}
-	duplicate = *head; /* on assigne head a duplicate */
-    /* on parcourt la liste jusqu'a la fin */
-    /* on assigne le nouveau node a la fin de la liste */
+	duplicate = *head; /* assigne head à duplicate */
 	while (duplicate->next)
 	{
 		duplicate = duplicate->next;
 	}
-    /* on assigne le nouveau node a la fin de la liste */
 	duplicate->next = new_node;
-    /* on retourne le nouveau node */
 	return (new_node);
 }
-
