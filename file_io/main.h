cb94b9f1e430f491b4ccea68ad97b25116a58064
L344S <7474@holbertonstudents.com>
2023-12-05 09:14:35 +0100

    Added main.h : Includes function prototypes and necessary libraries for the project

---
+++ file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* LIBRARIES */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+/* TASK 0 */
+ssize_t read_textfile(const char *filename, size_t letters);
+/* TASK 1 */
+int create_file(const char *filename, char *text_content);
+/* TASK 2 */
+int append_text_to_file(const char *filename, char *text_content);
+/* TASK 3 */
+int copy_file(const char *file_from, const char *file_to);
+
+#endif
