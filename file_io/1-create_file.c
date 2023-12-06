daeb608ca7210bce5ff1ca928b42a7243850e56a
L344S <7474@holbertonstudents.com>
2023-12-06 10:38:50 +0100

    Changed create_file function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,26 +7,41 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	/* sert à stocker le file descriptor, longueur et le nb de lettres écrites*/
+	int file_descriptor, lenght, write_count = 0;
+	mode_t permission =  S_IRUSR | S_IWUSR; /* Permission du fichier créé */
 
-	if (filename == NULL)
+	if (filename == NULL) /* ERR : Si le nom du fichier est NULL */
+		return (-1); /* On retourne -1 */
+
+	/* Ouverture du fichier en écriture seulement avec les perm r et w*/
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permission);
+
+	if (file_descriptor == -1) /* ERR : si l'ouverture échoue */
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
+	/* Calcul de la longueur du texte à ajouter */
+	for (lenght = 0; text_content[lenght]; lenght++)
+		;
 
-
-	while (text_content[i] != '\0')
+	if (text_content != NULL) /* ERR : Si le contenu est NULL */
 	{
-		i++;
+		/* Écriture du contenu dans le fichier */
+		write_count = write(file_descriptor, text_content, lenght);
+		if (write_count == -1) /* ERR : si l'écriture échoue */
+		{
+			close(file_descriptor); /* On ferme le fichier */
+			return (-1);
+		}
 	}
 
-	file = open(filename, O_WRONLY | O_APPEND);
-
-	if (file == -1)
+	if (write_count == -1) /* ERR : si l'écriture échoue */
+	{
+		close(file_descriptor); /* On ferme le fichier */
 		return (-1);
+	}
 
-	write(file, text_content, i);
+	close(file_descriptor); /* Fermeture du fichier */
 
-	return (1);
+	return (1); /* On retourne 1 pour indiquer le succès */
 }
