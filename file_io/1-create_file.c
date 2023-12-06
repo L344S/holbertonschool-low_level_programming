be75f4f33325eec62dc4125c439cdbff08ed03ff
L344S <7474@holbertonstudents.com>
2023-12-06 10:29:13 +0100

    Changed create_file function to replace _strlen with a for loop, added a var for permission

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,24 +7,27 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor; /* File descriptor pour le fichier créé */
-
-	ssize_t write_count = 0; /* Nombre d'octets écrits dans le fichier */
+	/* sert à stocker le file descriptor, longueur et le nb de lettres écrites*/
+	int file_descriptor, lenght, write_count = 0;
+	mode_t permission =  S_IRUSR | S_IWUSR; /* Permission du fichier créé */
 
 	if (filename == NULL) /* ERR : Si le nom du fichier est NULL */
 		return (-1); /* On retourne -1 */
 
 	/* Ouverture du fichier en écriture seulement avec les perm r et w*/
-	file_descriptor = open(filename, O_WRONLY | O_CREAT |
-	O_TRUNC, S_IRUSR | S_IWUSR);
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permission);
 
 	if (file_descriptor == -1) /* ERR : si l'ouverture échoue */
 		return (-1);
 
+	/* Calcul de la longueur du texte à ajouter */
+	for (lenght = 0; text_content[lenght]; lenght++)
+		;
+
 	if (text_content != NULL) /* ERR : Si le contenu est NULL */
 	{
 		/* Écriture du contenu dans le fichier */
-		write_count = write(file_descriptor, text_content, _strlen(text_content));
+		write_count = write(file_descriptor, text_content, lenght);
 		if (write_count == -1) /* ERR : si l'écriture échoue */
 		{
 			close(file_descriptor); /* On ferme le fichier */
