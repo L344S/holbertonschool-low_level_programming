2cac67a3e7c485f59b03098472381615d841e237
L344S <7474@holbertonstudents.com>
2023-12-06 10:10:23 +0100

    Changed function to make it Betty coding style friendly

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,60 +1,40 @@
 #include "main.h"
 /**
-* main - copies the content of a file to another file
-* @argc: The number of arguments passed
-* @argv: The arguments passed
-* Return: Always 0 (Success) or error code
-*/
-int main(int argc, char *argv[])
+  * main - copies the content of a file to another file
+  * @ac: number of arguments
+  * @av: array of arguments passed to function
+  * Return: 0 on success or exit with error code on failure
+  */
+int main(int ac, char *av[])
 {
-	int og_file_descriptor, copy_file_descriptor;
-	char buffer[1024]; /* stock tmp la data du fichier */
-	ssize_t read_count, write_count; /* nb de caractères lus et écrits */
+	int og_fd, cp_fd, read_count, write_count;
+	char buffer[1024];
+	mode_t permission =  0000200 | S_IRUSR | S_IWGRP | S_IROTH | S_IRGRP;
 
-	if (argc != 3) /* Si le nombre d'arguments est incorrect */
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	og_fd = open(av[1], O_RDONLY);
+	if (og_fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	cp_fd = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, permission);
+	if (cp_fd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	read_count = 1;
+	while (read_count)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97); /* ERR : affichage du message d'erreur et exit code 97 */
+		read_count = read(og_fd, buffer, 1024);
+		if (read_count == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		if (read_count > 0)
+		{
+			write_count = write(cp_fd, buffer, read_count);
+			if (write_count != read_count || write_count == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		}
 	}
-	og_file_descriptor = open(argv[1], O_RDONLY);
-	if (og_file_descriptor == -1) /* ERR : si l'ouverture échoue */
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	copy_file_descriptor = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (copy_file_descriptor == -1) /* ERR : si l'ouverture échoue */
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99); /* ERR : affichage du message d'erreur et exit code 99 */
-	}
-	while ((read_count = read(og_file_descriptor, buffer, sizeof(buffer))) > 0)
-	{
-	write_count = write(copy_file_descriptor, buffer, read_count);
-	if (write_count == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-	else if (write_count < read_count)
-	{
-		dprintf(STDERR_FILENO, "Warning: Incomplete write to %s\n", argv[2]);
-	}
-	}
-	if (read_count == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98); /* ERR : affichage du message d'erreur et exit code 98 */
-	}
-	if (close(og_file_descriptor) == -1) /* Fermeture du fichier original */
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", og_file_descriptor);
-		exit(100);
-	}
-	if (close(copy_file_descriptor) == -1) /* Fermeture du fichier copie */
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copy_file_descriptor);
-		exit(100);
-	}
-	return (0); /* On retourne 0 en cas de succès */
+	if (close(og_fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", og_fd), exit(100);
+	if (close(cp_fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cp_fd), exit(100);
+	return (0);
 }
