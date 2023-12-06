29a17402567789e4263c3818f8674b77f72cef40
L344S <7474@holbertonstudents.com>
2023-12-06 09:55:53 +0100

    Divide function in multiple functions to make the code shorter (Betty)

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * error_exit - Print error message and exit with specified error code.
+ * print_error_and_exit - Print error message and exit with specified code.
  * @message: The error message to print.
  * @exit_code: The exit code.
  */
-void error_exit(const char *message, int exit_code)
+void print_error_and_exit(const char *message, int exit_code)
 {
 	dprintf(STDERR_FILENO, "Error: %s\n", message);
 	exit(exit_code);
@@ -13,24 +13,28 @@ void error_exit(const char *message, int exit_code)
 
 /**
  * copy_file - Copy content from one file to another.
- * @og_fd: Source file descriptor.
- * @cp_fd: Destination file descriptor.
+ * @src_fd: Source file descriptor.
+ * @dest_fd: Destination file descriptor.
  */
-void copy_file(int og_fd, int cp_fd)
+void copy_file(int src_fd, int dest_fd)
 {
 	char buffer[1024];
 	ssize_t read_count, write_count;
 
-	while ((read_count = read(og_fd, buffer, sizeof(buffer))) > 0)
+	while ((read_count = read(src_fd, buffer, sizeof(buffer))) > 0)
 	{
-		write_count = write(cp_fd, buffer, read_count);
+		write_count = write(dest_fd, buffer, read_count);
 		if (write_count == -1)
-			error_exit("Can't write to file", 99);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to file\n");
+			exit(99);
+		}
 		else if (write_count < read_count)
-			dprintf(STDERR_FILENO, "Warning: Incomplete write\n");
+			dprintf(STDERR_FILENO, "Warning: Incomplete write to file\n");
 	}
+
 	if (read_count == -1)
-		error_exit("Can't read from file", 98);
+		print_error_and_exit("Can't read from source file", 98);
 }
 
 /**
@@ -41,23 +45,23 @@ void copy_file(int og_fd, int cp_fd)
  */
 int main(int argc, char *argv[])
 {
-	int og_fd, cp_fd;
+	int src_fd, dest_fd;
 
 	if (argc != 3)
-		error_exit("Usage: cp file_from file_to", 97);
+		print_error_and_exit("Usage: cp file_from file_to", 97);
 
-	og_fd = open(argv[1], O_RDONLY);
-	if (og_fd == -1)
-		error_exit("Can't read from source file", 98);
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
+		print_error_and_exit("Can't read from source file", 98);
 
-	cp_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (cp_fd == -1)
-		error_exit("Can't write to destination file", 99);
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dest_fd == -1)
+		print_error_and_exit("Can't write to destination file", 99);
 
-	copy_file(og_fd, cp_fd);
+	copy_file(src_fd, dest_fd);
 
-	if (close(og_fd) == -1 || close(cp_fd) == -1)
-		error_exit("Can't close file descriptor", 100);
+	if (close(src_fd) == -1 || close(dest_fd) == -1)
+		print_error_and_exit("Can't close file descriptor", 100);
 
 	return (0);
 }
