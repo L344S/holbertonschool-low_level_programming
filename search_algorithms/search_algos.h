97582eea78792220ea890deec5e6f9054b09a5e9
L344S <7474@holbertonstudents.com>
2024-01-16 14:13:01 +0100

    Added search_algos.h (header file)
    Contains librairies and functions prototypes

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SALGO_H
+#define SALGO_H
+
+/* ---------- Libraries ---------- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/* ---------- Prototypes ---------- */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
