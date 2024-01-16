db64a53d6b90ec37f43b1fdeedff2a081d492df1
L344S <7474@holbertonstudents.com>
2024-01-16 14:16:42 +0100

    Added comments in search_algos.h
    Contains librairies and functions prototypes

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-#ifndef SALGO_H
-#define SALGO_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
 /* ---------- Libraries ---------- */
 #include <stdio.h>
@@ -7,7 +7,9 @@
 #include <stddef.h>
 
 /* ---------- Prototypes ---------- */
+/* 0-linear.c */
 int linear_search(int *array, size_t size, int value);
+/* 1-binary.c */
 int binary_search(int *array, size_t size, int value);
 
 
